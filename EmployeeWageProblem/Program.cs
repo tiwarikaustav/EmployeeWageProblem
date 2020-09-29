6a38a040cbcc92b18491deec7037d6b1d07285af
Aryav <tiwarikaustav@gmail.com>
2020-09-29 21:10:29 +0530

    Modified code to look clean!

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,23 +4,19 @@ namespace Employee_Wage_Problem
 {
     class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            //commenting
-            static void Main(string[] args)
-            {
-                int isPresent = Attendance();
-                if (isPresent == 0)
-                    Console.WriteLine("Employee is Absent!");
-                else
-                    Console.WriteLine("Employee is Present!");
-            }
-
-            static int Attendance()
-            {
-                Random randObj = new Random();
-                return randObj.Next(0, 2);
-            }
+            int isPresent = 0;
+            isPresent = Attendance();
+            if (isPresent != 0)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+        static int Attendance()
+        {
+            Random randObj = new Random();
+            return randObj.Next(0, 2);
         }
     }
 }
