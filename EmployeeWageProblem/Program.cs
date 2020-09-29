e3cd932eba0faa54da5c143dfb1b2fc247455240
Aryav <tiwarikaustav@gmail.com>
2020-09-29 21:26:52 +0530

    Used Methods

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,25 +6,26 @@ namespace Employee_Wage_Problem
     {
         static void Main(string[] args)
         {
-            int isPresent = Attendance();
-            if (isPresent == 0)
-                Console.WriteLine("Employee is Absent!");
+            int isPresent = 0;
+            isPresent = Attendance();
+            if(isPresent != 0)
+                Console.WriteLine("Employee is Present");
             else
-                Console.WriteLine("Employee is Present!");
+                Console.WriteLine("Employee is Absent");
             int dailyEmployeeWage = 0;
             if (isPresent == 1)
             {
                 dailyEmployeeWage = DailyEmployeeWage();
             }
-            Console.WriteLine("Daily Employee Wage : {0}", dailyEmployeeWage);
+            Console.WriteLine("Daily Wage of Employee is: {0}", dailyEmployeeWage);
         }
-
+        
         static int Attendance()
         {
             Random randObj = new Random();
             return randObj.Next(0, 2);
         }
-
+        
         static int DailyEmployeeWage()
         {
             int fullDayHour = 8;
